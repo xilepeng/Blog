2151a351abca734222d2b129bc07e840124af2b3
好饭不怕晚 <lepengxi@gmail.com>
2022-09-24 21:11:10 +0800

    db

--- model/db.go
+++ model/db.go
@@ -1,9 +1,15 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"fmt"
+
+	"github.com/xilepeng/Blog/utils"
+	"gorm.io/gorm"
+)
 
 var db *gorm.DB
+var err error
 
 func InitDb() {
-
+	db, err = gorm.Open(utils.Db,fmt.Sprintf("%s:%s@tcp(%s:%s/)"))
 }
